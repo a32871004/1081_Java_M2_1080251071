f5dd26c7c59a6d4c4c0d17c0ec34c78dd04b1b99
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-23 16:01:03 +0800

    M2Q14

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sn = new Scanner(System.in);
-        float n1 = sn.nextFloat();
-        float max = n1;
-        float min = n1;
-        for (int i = 1; i < 10; i++) {
-            float n2 = sn.nextFloat();
-            if (max < n2) {
-                max = n2;
-            } else if (min > n2) {
-                min = n2;
-            }
+        Scanner sn=new Scanner(System.in);
+        int n=sn.nextInt();
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum=sum+i*(i+1);
         }
-        System.out.print("Max=");
-        System.out.printf("%.2f\n" ,max);
-        System.out.print("Min=");
-        System.out.printf("%.2f\n",min);
+        System.out.println(sum);
     }
 }
