1d976e1c9301b8e749630b7613c3ced23e4c66a9
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-23 15:01:43 +0800

    M2Q1

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sn=new Scanner(System.in);
-        int a=sn.nextInt();
-        int sum=0;
-        for(int i=3;i<=a;i+=3){
-        sum=sum+i;}
-        System.out.println(sum);
+        float n1=sn.nextFloat();
+        float max=n1;
+        float min=n1;
+        for (int i=1;i<10;i++){
+            float n2=sn.nextFloat();
+            if(max<n2){
+                max=n2;
+            }
+            else if(min>n2){
+                min=n2;
+            }
+        }
+        System.out.println("max="+max);
+        System.out.println("min="+min);
     }
 }
